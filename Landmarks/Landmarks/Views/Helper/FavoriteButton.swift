3c05bb065dc4c3bdebf30ac021b0e8a2c6eaae83
Filipe Patricio <facpatricio@gmail.com>
2023-12-01 14:43:37 +0100

    Complete Section 6

---
+++ Landmarks/Landmarks/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,25 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Filipe Patricio on 29/11/2023.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
