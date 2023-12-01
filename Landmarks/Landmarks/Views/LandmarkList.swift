56e78cd84fa9f466b25a62729f950ece929e9834
Filipe Patricio <facpatricio@gmail.com>
2023-12-01 14:39:27 +0100

    Complete Section 3

--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -11,18 +11,23 @@ struct LandmarkList: View {
     @State private var showFavoritesOnly = false
     
     var filteredLandmarks: [Landmark] {
-        landmarks.filter {
-            landmark in (!showFavoritesOnly || landmark.isFavorite)
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
         }
     }
     
     var body: some View {
         NavigationSplitView {
-            List (filteredLandmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly){
+                    Text("Favorites only")
+                }
+                ForEach(filteredLandmarks){ landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
