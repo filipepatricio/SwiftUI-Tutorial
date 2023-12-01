ff6b6c4cb910c45e3badd94005a6eaaf01bcf4fa
Filipe Patricio <facpatricio@gmail.com>
2023-12-01 14:22:05 +0100

    Start Landmarks Project

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Filipe Patricio on 28/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
