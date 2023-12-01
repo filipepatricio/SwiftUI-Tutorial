a08199f8695bb458fd525aedfdf9b961755d1468
Filipe Patricio <facpatricio@gmail.com>
2023-12-01 14:41:52 +0100

    Complete Section 4

--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -11,7 +11,7 @@ struct LandmarkList: View {
     @State private var showFavoritesOnly = false
     
     var filteredLandmarks: [Landmark] {
-        landmarks.filter { landmark in
+        ModelData().landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
         }
     }
