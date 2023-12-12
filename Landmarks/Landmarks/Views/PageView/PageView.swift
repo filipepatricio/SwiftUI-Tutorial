ca1c9e762a1425600529d7c2ab34c267e77156ef
Filipe Patricio <facpatricio@gmail.com>
2023-12-12 15:48:08 +0100

    Complete Section 3

--- Landmarks/Landmarks/Views/PageView/PageView.swift
+++ Landmarks/Landmarks/Views/PageView/PageView.swift
@@ -9,10 +9,18 @@ import SwiftUI
 
 struct PageView<Page: View>: View {
     var pages: [Page]
+    @State private var currentPage = 0
     
     var body: some View {
-        PageViewController(pages: pages)
-            .aspectRatio(3 / 2, contentMode: .fit)
+        VStack {
+            PageViewController(pages: pages, currentPage: $currentPage)
+                .aspectRatio(3 / 2, contentMode: .fit)
+            Button("next", action: {
+                currentPage = currentPage == pages.count - 1 ? 0 : currentPage + 1
+            })
+            Text("Current Page: \(currentPage)")
+        }
+        .aspectRatio(3 / 2, contentMode: .fit)
     }
 }
 
--- Landmarks/Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Landmarks/Views/PageView/PageViewController.swift
@@ -9,30 +9,32 @@ import SwiftUI
 import UIKit
 
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+    @Binding var currentPage: Int
+    
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-    
-    var pages: [Page]
     
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
         pageViewController.dataSource = context.coordinator
-
+        pageViewController.delegate = context.coordinator
+        
         return pageViewController
     }
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [context.coordinator.controllers[0]], direction: .forward, animated: true)
+            [context.coordinator.controllers[currentPage]], direction: .forward, animated: true)
     }
     
-    class Coordinator: NSObject, UIPageViewControllerDataSource {
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
         var parent: PageViewController
         var controllers = [UIViewController]()
-
+        
         init(_ pageViewController: PageViewController) {
             parent = pageViewController
             controllers = parent.pages.map { UIHostingController(rootView: $0) }
@@ -50,7 +52,7 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             }
             return controllers[index - 1]
         }
-
+        
         func pageViewController(
             _ pageViewController: UIPageViewController,
             viewControllerAfter viewController: UIViewController) -> UIViewController?
@@ -64,5 +66,17 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             
             return controllers[index + 1]
         }
+        
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            didFinishAnimating finished: Bool,
+            previousViewControllers: [UIViewController],
+            transitionCompleted completed: Bool) {
+                if completed,
+                   let visibleViewController = pageViewController.viewControllers?.first,
+                   let index = controllers.firstIndex(of: visibleViewController) {
+                    parent.currentPage = index
+                }
+            }
     }
 }
