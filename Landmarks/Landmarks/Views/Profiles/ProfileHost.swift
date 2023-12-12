a7481005494ea6ea9c019d1c060c7f096a44e211
Filipe Patricio <facpatricio@gmail.com>
2023-12-12 11:50:08 +0100

    Start Working with UI controls - Complete Section 1

---
+++ Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Filipe Patricio on 12/12/2023.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
