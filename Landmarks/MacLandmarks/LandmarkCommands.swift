9c00c454f4e4256943131939165e543e301cda90
Filipe Patricio <facpatricio@gmail.com>
2023-12-14 13:07:00 +0100

    Complete Section 5

---
+++ Landmarks/MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by Filipe Patricio on 14/12/2023.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
